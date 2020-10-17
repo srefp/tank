4a83318a372c7c911a255067ef1e893be2380565
test_fault <1903994095@qq.com>
2020-10-17 12:40:33 +0800

    update java source file

--- tank_mashibing/src/com/mashibing/tank/T.java
+++ tank_mashibing/src/com/mashibing/tank/T.java
@@ -7,5 +7,6 @@ import java.awt.event.WindowEvent;
 public class T {
     public static void main(String[] args) {
         TankFrame tankFrame = new TankFrame();
+        System.out.println("hello, github2!");
     }
 }
