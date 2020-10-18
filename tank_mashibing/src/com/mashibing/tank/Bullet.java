aa1f5e88af1c6a61df332e13c4e609b2be2fcf61
test_fault <1903994095@qq.com>
2020-10-18 16:10:07 +0800

    消除子弹列表的内存泄漏问题，小心处理迭代器中的删除问题。

--- tank_mashibing/src/com/mashibing/tank/Bullet.java
+++ tank_mashibing/src/com/mashibing/tank/Bullet.java
@@ -3,19 +3,26 @@ package com.mashibing.tank;
 import java.awt.*;
 
 public class Bullet {
-    private static final int SPEED = 1;
+    private static final int SPEED = 10;
     private static int WIDTH = 30, HEIGHT = 30;
 
     private int x, y;
     private Dir dir;
 
-    public Bullet(int x, int y, Dir dir) {
+    private boolean live = true;
+    TankFrame tf = null;
+
+    public Bullet(int x, int y, Dir dir, TankFrame tf) {
         this.x = x;
         this.y = y;
         this.dir = dir;
+        this.tf = tf;
     }
 
     public void paint(Graphics g) {
+        if (!live) {
+            tf.bullets.remove(this);
+        }
         Color c = g.getColor();
         g.setColor(Color.RED);
         g.fillOval(x, y, WIDTH, HEIGHT);
@@ -39,5 +46,7 @@ public class Bullet {
                 y += SPEED;
                 break;
         }
+
+        if (x < 0 || y < 0 || x > TankFrame.GAME_WIDTH || y > TankFrame.GAME_HEIGHT) live = false;
     }
 }
--- tank_mashibing/src/com/mashibing/tank/TankFrame.java
+++ tank_mashibing/src/com/mashibing/tank/TankFrame.java
@@ -2,11 +2,13 @@ package com.mashibing.tank;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TankFrame extends Frame {
 
     Tank myTank = new Tank(200, 200, Dir.DOWN, this);
-    Bullet b = new Bullet(300, 300, Dir.DOWN);
+    List<Bullet> bullets = new ArrayList<>();
     static final int GAME_WIDTH = 800, GAME_HEIGHT = 600;
 
     public TankFrame() {
@@ -42,8 +44,16 @@ public class TankFrame extends Frame {
 
     @Override
     public void paint(Graphics g) {
+
+        Color c = g.getColor();
+        g.setColor(Color.WHITE);
+
+        g.drawString("子弹的数量：" + bullets.size(), 10, 60);
+        g.setColor(c);
         myTank.paint(g);
-        b.paint(g);
+        for (int i = 0; i < bullets.size(); i++) {
+            bullets.get(i).paint(g);
+        }
     }
 
     class MyKeyListener extends KeyAdapter {
