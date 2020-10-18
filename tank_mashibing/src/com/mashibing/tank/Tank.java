25069a921cbcabe15efc29b5e899059e0d35a3d1
test_fault <1903994095@qq.com>
2020-10-18 13:58:55 +0800

    添加坦克静止的处理，只有四个键都没有按下，坦克就是静止的。

--- tank_mashibing/src/com/mashibing/tank/Tank.java
+++ tank_mashibing/src/com/mashibing/tank/Tank.java
@@ -6,6 +6,15 @@ public class Tank {
     private int x, y;
     private Dir dir = Dir.DOWN;
     private static final int SPEED = 10;
+    private boolean moving = false;
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
 
     public Dir getDir() {
         return dir;
@@ -23,6 +32,15 @@ public class Tank {
 
     public void paint(Graphics g) {
         g.fillRect(x, y, 50, 50);
+
+        move();
+
+
+    }
+
+    private void move() {
+        if (!moving) return;
+
         switch (dir) {
             case LEFT:
                 x -= SPEED;
