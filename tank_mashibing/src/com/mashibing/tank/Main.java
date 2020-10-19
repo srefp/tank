c740c502c827b6c5d1c77d2f151d3a68cea0fe02
test_fault <1903994095@qq.com>
2020-10-19 09:25:56 +0800

    new出5个敌方坦克。

--- tank_mashibing/src/com/mashibing/tank/Main.java
+++ tank_mashibing/src/com/mashibing/tank/Main.java
@@ -4,9 +4,14 @@ import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
-public class T {
+public class Main {
     public static void main(String[] args) throws InterruptedException {
         TankFrame tf = new TankFrame();
+
+        //初始化地方坦克
+        for (int i = 0; i < 5; i++) {
+            tf.tanks.add(new Tank(50 + i * 80, 200, Dir.DOWN, tf));
+        }
         while (true) {
             Thread.sleep(50);
             tf.repaint();
